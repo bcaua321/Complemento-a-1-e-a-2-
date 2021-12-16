49628eca55ad66be58699ed90901220e1771e55f
Breno <bcaua205@gmail.com>
2021-12-16 00:12:11 -0300

    removendo comentarios

--- Complements/complementTwo.js
+++ Complements/complementTwo.js
@@ -1,11 +1,11 @@
-// Complemento a dois irá add o bit 1 ao complemento a 2
+// Complemento a dois irá add o bit 1 ao complemento a 1
 exports.complement = (n) => {
   let arr = [...n]; // Para copiar todos os elementos do array, assim não será passado por referência
 
   // Variavel para iterar
   i = arr.length - 1;
 
-  // não se sabe onde irá parar
+
   while(i != -1){
     if(arr[i] == 0){
       arr[i] = 1;
